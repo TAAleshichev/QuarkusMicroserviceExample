34abc6e37077d41869b791002d097c45a109ac2a
TAAleshichev <62710971+TAAleshichev@users.noreply.github.com>
2022-10-07 22:07:04 +0300

    Update import.sql

--- hibernate-orm-panache-quickstart/src/main/resources/import.sql
+++ hibernate-orm-panache-quickstart/src/main/resources/import.sql
@@ -1,4 +1,9 @@
-INSERT INTO known_fruits(id, name) VALUES (nextval('hibernate_sequence'), 'Cherry');
-INSERT INTO known_fruits(id, name) VALUES (nextval('hibernate_sequence'), 'Apple');
-INSERT INTO known_fruits(id, name) VALUES (nextval('hibernate_sequence'), 'Banana');
+INSERT INTO meal_types(id, name) VALUES (nextval('SQ_MEALTYPE_SEQ'),'Завтрак');
+INSERT INTO meal_types(id,name) VALUES (nextval('SQ_MEALTYPE_SEQ'),'Обед');
+INSERT INTO meal_types(id,name) VALUES (nextval('SQ_MEALTYPE_SEQ'),'Ужин');
+
+INSERT INTO dishes(id,name, cost) VALUES (nextval('SQ_DISH_SEQ'),'Борщ', 150);
+INSERT INTO dishes(id,name, cost) VALUES (nextval('SQ_DISH_SEQ'),'Картофельное пюре', 50);
+INSERT INTO dishes(id,name, cost) VALUES (nextval('SQ_DISH_SEQ'),'Куриная Котлета', 45);
+INSERT INTO dishes(id,name, cost) VALUES (nextval('SQ_DISH_SEQ'),'Морс', 30.50);
 
